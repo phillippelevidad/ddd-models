efc7ed3bf5123528e139854d1cc33089a77bade5
Phillippe Santana <phillippelevidad@gmail.com>
2019-07-03 13:59:06 -0300

    Add domain event handling to publish an integration event.

---
+++ Core/IDomainEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public interface IDomainEventHandler<in TDomainEvent> where TDomainEvent : IDomainEvent
+    {
+        Task<Result> HandleAsync(TDomainEvent domainEvent, CancellationToken cancellationToken);
+    }
+}
---
+++ ShoppingCart/ShoppingCart/Application/PublishIntegrationEventWhenCartIsClosedForCheckout.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+using CSharpFunctionalExtensions;
+using ShoppingCart.Model;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Application
+{
+    public class PublishIntegrationEventWhenCartIsClosedForCheckout : IDomainEventHandler<CartClosedForCheckout>
+    {
+        public Task<Result> HandleAsync(CartClosedForCheckout domainEvent, CancellationToken cancellationToken)
+        {
+            // TODO: publish an integration event to start the checkout process
+            throw new NotImplementedException();
+        }
+    }
+}
